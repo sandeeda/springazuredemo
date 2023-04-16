f3f93700fec5586d3edde11bd38f984109ac526c
Sandeep Das <sdsts28@outlook.com>
2023-04-16 14:19:33 -0400

    first commit

---
+++ src/main/java/com/sandeep/spring/azure/demo/SpringAzureDemoApplication.java
@@ -0,0 +1,20 @@
+package com.sandeep.spring.azure.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAzureDemoApplication {
+
+	@GetMapping("/message")
+	public String message() {
+		return "Congrats";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemoApplication.class, args);
+	}
+
+}
