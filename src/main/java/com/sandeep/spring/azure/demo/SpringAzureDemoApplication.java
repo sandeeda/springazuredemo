3d59b00f8e44255437708bdb9aed1b541c9ca982
Sandeep Das <sdsts28@outlook.com>
2023-04-16 14:42:47 -0400

    adding tax calculator

--- src/main/java/com/sandeep/spring/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/sandeep/spring/azure/demo/SpringAzureDemoApplication.java
@@ -15,6 +15,49 @@ public class SpringAzureDemoApplication {
 	    return htmlContent;
 	}
 
+	@GetMapping("/")
+	public String calculateTax() {
+	    String htmlContent = "<html>\n" +
+                "<head>\n" +
+                "<title>Tax Calculator</title>\n" +
+                "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css\">\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<div class=\"container mt-5\">\n" +
+                "<h1>Tax Calculator</h1>\n" +
+                "<form>\n" +
+                "<div class=\"form-group\">\n" +
+                "<label for=\"income\">Income:</label>\n" +
+                "<input type=\"number\" class=\"form-control\" id=\"income\" name=\"income\" required>\n" +
+                "</div>\n" +
+                "<div class=\"form-group\">\n" +
+                "<label for=\"taxRate\">Tax Rate:</label>\n" +
+                "<input type=\"number\" class=\"form-control\" id=\"taxRate\" name=\"taxRate\" required>\n" +
+                "</div>\n" +
+                "<button type=\"button\" class=\"btn btn-primary mr-2\" onclick=\"calculateTax()\">Calculate Tax</button>\n" +
+                "<button type=\"button\" class=\"btn btn-secondary\" onclick=\"resetForm()\">Reset</button>\n" +
+                "</form>\n" +
+                "<div id=\"result\" class=\"mt-3\"></div>\n" +
+                "</div>\n" +
+                "<script>\n" +
+                "function calculateTax() {\n" +
+                "    var income = document.getElementById('income').value;\n" +
+                "    var taxRate = document.getElementById('taxRate').value;\n" +
+                "    var tax = income * (taxRate / 100);\n" +
+                "    var result = document.getElementById('result');\n" +
+                "    result.innerHTML = '<p>Tax: $' + tax.toFixed(2) + '</p>';\n" +
+                "}\n" +
+                "function resetForm() {\n" +
+                "    document.getElementById('income').value = '';\n" +
+                "    document.getElementById('taxRate').value = '';\n" +
+                "    document.getElementById('result').innerHTML = '';\n" +
+                "}\n" +
+                "</script>\n" +
+                "</body>\n" +
+                "</html>";
+	    return htmlContent;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAzureDemoApplication.class, args);
 	}
